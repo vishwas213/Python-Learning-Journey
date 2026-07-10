79f63fded13361f01aa4a7021f67510996009b04
vishwas saini <105575432+vishwas213@users.noreply.github.com>
2026-07-11 01:36:34 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,14 @@
+# num = int(input("enter number :"))
+
+# print ("number is even ",num % 2 == 0) 
+#  this is what i try myself
+
+
+# this is after learn
+
+num = int(input("enter you number :"))
+
+if num % 2 == 0:
+    print("number is even " )
+else :
+    print(" number is odd")

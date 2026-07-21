787463b1db8a09a17aad5014f3848b05aae87c91
vishwas saini <105575432+vishwas213@users.noreply.github.com>
2026-07-21 16:26:56 +0530

    Update main.py

--- week one project/main.py
+++ week one project/main.py
@@ -1,1 +1,67 @@
+students = []
 
+while True:
+
+    print("\n=====================")
+    print(" Student Result System")
+    print("=====================")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Exit")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 1:
+
+        name = input("Enter Name: ")
+        roll = int(input("Enter Roll Number: "))
+        marks = int(input("Enter Marks: "))
+
+        student = {
+            "name": name,
+            "roll": roll,
+            "marks": marks
+        }
+
+        students.append(student)
+
+        print("Student Added Successfully!")
+
+    elif choice == 2:
+
+        if len(students) == 0:
+            print("No students found.")
+
+        else:
+            for student in students:
+                print("-----------------")
+                print("Name :", student["name"])
+                print("Roll :", student["roll"])
+                print("Marks:", student["marks"])
+
+    elif choice == 3:
+
+        search = input("Enter Name: ")
+
+        found = False
+
+        for student in students:
+            if student["name"] == search:
+                print("-----------------")
+                print("Name :", student["name"])
+                print("Roll :", student["roll"])
+                print("Marks:", student["marks"])
+                found = True
+                break
+
+        if found == False:
+            print("Student Not Found")
+
+    elif choice == 4:
+
+        print("Thank You!")
+        break
+
+    else:
+        print("Invalid Choice")
